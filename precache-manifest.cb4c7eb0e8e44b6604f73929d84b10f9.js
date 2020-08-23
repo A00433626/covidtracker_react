7204816a971860e0a73773a3ecacc1fd7ecf9479
A00433626 <Vidhyadhara.Teja.Katragadda@smu.ca>
2020-08-23 16:26:39 -0300

    Updates

---
+++ precache-manifest.cb4c7eb0e8e44b6604f73929d84b10f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5629168f340c7d00dcd8b9e5c43da06",
+    "url": "/covidtracker_react/index.html"
+  },
+  {
+    "revision": "3b87fa9e030191bee227",
+    "url": "/covidtracker_react/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "4c5726392d43199a9250",
+    "url": "/covidtracker_react/static/css/main.86a6cd37.chunk.css"
+  },
+  {
+    "revision": "3b87fa9e030191bee227",
+    "url": "/covidtracker_react/static/js/2.6cda8563.chunk.js"
+  },
+  {
+    "revision": "a44155bfdb6c9dd7cfa66d662e6de04b",
+    "url": "/covidtracker_react/static/js/2.6cda8563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c5726392d43199a9250",
+    "url": "/covidtracker_react/static/js/main.6472246c.chunk.js"
+  },
+  {
+    "revision": "13c61ddf0787515a4ff9",
+    "url": "/covidtracker_react/static/js/runtime-main.aaa58931.js"
+  }
+]);
